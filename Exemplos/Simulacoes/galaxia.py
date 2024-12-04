8959d5e8f9a7ea3d0a584f17f3eb9378312820ed
Alec Aoki <alec.camposaoki@gmail.com>
2024-12-04 16:07:57 -0300

    Update galaxy

--- Exemplos/Simulacoes/galaxia.py
+++ Exemplos/Simulacoes/galaxia.py
@@ -1,214 +1,401 @@
-import pygame
+import pygame as pg
+import pygame_gui as pg_gui
 import math
 import random
 
-# Inicialização do Pygame
-pygame.init()
-LARGURA, ALTURA = 950, 950
-tela = pygame.display.set_mode((LARGURA, ALTURA))
-clock = pygame.time.Clock()
+### PARÂMETROS DA SIMULAÇÃO ###
+G = 6.67430 # Constante gravitacional com magnitude menor
 
-# Constantes
-G = 6.67430  # Constante gravitacional
-NUM_ESTRELAS = 2
-NUM_PLANETAS = 10000
-RAIO_PROPORCIONALIDADE = 3  # Constante de ajuste para o raio
+# Estrelas
+RAIO_ESTRELA = 80
+COR_ESTRELA = (255, 165, 0) #Laranja
+QUANT_ESTRELAS = 2 # Quantidade inicial padrão de estrelas
+# Range da velocidade das estrelas
+LIM_INF_VEL_ESTRELA = 0
+LIM_SUP_VEL_ESTRELA = 0
+# Range da massa da estrela
+LIM_INF_MASSA_ESTRELA = 1
+LIM_SUP_MASSA_ESTRELA = 1
 
-# Espaço virtual
-ESCALA = 500  # Fator de escala: o mundo será 10x maior que a janela
-ESPACO_VIRTUAL_LARGURA = LARGURA * ESCALA
-ESPACO_VIRTUAL_ALTURA = ALTURA * ESCALA
+# Planetas
+RAIO_PLANETA = 45
+COR_PLANETA = (144, 238, 144) # Verde
+QUANT_PLANETAS = 1000000 # Quantidade inicial padrão de planetas
+# Range da velocidade dos planetas
+LIM_INF_VEL_PLANETA = 0
+LIM_SUP_VEL_PLANETA = 0
+# Range da massa da estrela
+LIM_INF_MASSA_PLANETA = 1
+LIM_SUP_MASSA_PLANETA = 1
+### CONSTANTES DA SIMULAÇÃO ###
 
-# Funções auxiliares
-def calcular_raio(massa):
-    return (massa ** (1/3)) * RAIO_PROPORCIONALIDADE
+pg.init() # Inicializando o pygame
 
-def gerar_posicao_aleatoria():
-    x = random.uniform(40000, 120000)
-    y = random.uniform(300000, 400000)
-    return x, y
+### CONFIGURAÇÕES DA JANELA/GUI ###
+LARGURA, ALTURA = 1250, 700 # Dimensões da janela
+# Configurações do espaço virtual
+ESCALA = 20
+ESPACO_VIRT_LARG = LARGURA * ESCALA
+ESPACO_VIRT_ALT = ALTURA * ESCALA
 
+tela = pg.display.set_mode((LARGURA, ALTURA))
+pg.display.set_caption("Simulação Espaço") # Título da janela
+clock = pg.time.Clock() # Inicializando o clock para controlar o FPS
 
-# Classes para Estrelas e Planetas
+gui = pg_gui.UIManager((LARGURA, ALTURA)) # Inicializando a GUI
+
+# Configuração dos "rótulos" (onde vão os sliders)
+rot_estrelas = pg_gui.elements.UILabel(
+  relative_rect = pg.Rect((10, 10), (250, 50)), # Posição e tamanho do rótulo
+  text = "Quantidade de Estrelas", # Exibe a quantidade de estrelas atualmente selecionada
+  manager = gui, 
+)
+
+rot_planetas = pg_gui.elements.UILabel(
+  relative_rect = pg.Rect((10, 110), (250, 30)), # Posição e tamanho do rótulo
+  text = "Quantidade de Planetas", # Exibe a quantidade de planetas atualmente selecionada
+  manager = gui, 
+)
+
+rot_slider_estrelas = pg_gui.elements.UILabel(
+  relative_rect = pg.Rect((220, 10), (100, 50)), # Posição e tamanho do rótulo
+  text = str(QUANT_ESTRELAS), # Exibe a quantidade de estrelas atualmente selecionada
+  manager = gui, 
+)
+
+rot_slider_planetas = pg_gui.elements.UILabel(
+  relative_rect = pg.Rect((220, 110), (100, 50)), # Posição e tamanho do rótulo
+  text = str(QUANT_PLANETAS), # Exibe a quantidade de planetas atualmente selecionada
+  manager = gui, 
+)
+
+# Sliders
+slider_estrelas = pg_gui.elements.UIHorizontalSlider(
+  relative_rect = pg.Rect((10, 50), (200, 50)), # Posição e tamanho do rótulo
+  start_value = QUANT_ESTRELAS, # Quantidade inicial padrão de estrelas
+  value_range = (0, 1000), # Limites do slider
+  manager = gui,
+)
+
+slider_planetas = pg_gui.elements.UIHorizontalSlider(
+  relative_rect = pg.Rect((10, 150), (200, 50)), # Posição e tamanho do rótulo
+  start_value = QUANT_PLANETAS, # Quantidade inicial padrão de planetas
+  value_range = (0, 1000), # Limites do slider
+  manager = gui,
+)
+
+# Botão de inicar
+botao_iniciar = pg_gui.elements.UIButton(
+  relative_rect = pg.Rect((10, 220), (100, 50)), # Posição e tamanho do botão
+  text = "Iniciar", # Texto dentro do botão
+  manager = gui,
+)
+### CONFIGURAÇÕES DA JANELA/GUI ###
+
+### CLASSE DAS ESTRELAS E DOS PLANETAS ###
 class CorpoCeleste:
-    def __init__(self, x, y, vx, vy, massa, raio):
-        self.x = x
-        self.y = y
-        self.vx = vx
-        self.vy = vy
-        self.massa = massa
-        self.raio = raio
-        self.ativo = True
+  """
+  Classe base para representar corpos celestes genéricos
+    
+  Atributos:
+    x (float): Coordenada x no espaço
+    y (float): Coordenada y no espaço
+    vx (float): Velocidade no eixo x
+    vy (float): Velocidade no eixo y
+    massa (float): Massa do corpo celeste
+    raio (float): Raio do corpo celeste
+    ativo (bool): Estado do corpo (True se ativo, False se desativado)
+    cor (tuple): Cor do corpo no formato RGB
+    """
+
+  def __init__(self, x: float, y: float, vx: float, vy: float, massa: float, raio: float, cor: tuple):
+    self.x = x
+    self.y = y
+    self.vx = vx
+    self.vy = vy
+    self.massa = massa
+    self.raio = raio
+    self.ativo = True
+    self.cor = cor
+
 
 class Estrela(CorpoCeleste):
-    def __init__(self, x, y, vx, vy, massa, raio):
-        super().__init__(x, y, vx, vy, massa, raio)
+  """
+  Classe que representa uma estrela, herdando de CorpoCeleste.
+    
+  Utiliza os mesmos atributos e comportamento de CorpoCeleste.
+  """
+
+  def __init__(self, x: float, y: float, vx: float, vy: float, massa: float, raio: float, cor: tuple):
+    super().__init__(x, y, vx, vy, massa, raio, cor)
+
 
 class Planeta(CorpoCeleste):
-    def __init__(self, x, y, vx, vy, massa, raio):
-        super().__init__(x, y, vx, vy, massa, raio)
+  """
+  Classe que representa um planeta, herdando de CorpoCeleste.
+    
+  Utiliza os mesmos atributos e comportamento de CorpoCeleste.
+  """
 
-# Função para criar estrelas
-def criar_estrelas(num_estrelas):
-    estrelas = []
-
-    x = (ESPACO_VIRTUAL_LARGURA/2)+30000 
-    y = (ESPACO_VIRTUAL_ALTURA/2)-15000
-    massa = 2000000000
-    raio = calcular_raio(massa)
-    estrelas.append(Estrela(x, y,  -150, -150, massa, raio))
+  def __init__(self, x: float, y: float, vx: float, vy: float, massa: float, raio: float, cor: tuple):
+    super().__init__(x, y, vx, vy, massa, raio, cor)
+### CLASSE DAS ESTRELAS E DOS PLANETAS ###
 
 
-    x = (ESPACO_VIRTUAL_LARGURA/2)-30000 
-    y = (ESPACO_VIRTUAL_ALTURA/2)+15000
-    massa = 2000000000
-    raio = calcular_raio(massa)
-    estrelas.append(Estrela(x, y,  150, 150, massa, raio))
+### FUNÇÕES ###
+def atualizar_informacoes():
+  """
+  Atualiza a quantidade de estrelas e planetas (globalmente) com base no valor inserido no slider
+  """
+  global QUANT_ESTRELAS, QUANT_PLANETAS
+  QUANT_ESTRELAS = int(slider_estrelas.get_current_value())
+  QUANT_PLANETAS = int(slider_planetas.get_current_value())
+
+def gerar_posicao_aleatoria() -> tuple:
+  """
+  Gera coordenadas (x, y) aleatórias dentro do espaço virtual definido
+
+  Parâmetros:
+  
+  Retorno:
+    x, y (tuple): Coordendas (x, y)
+  """
+
+  x = random.uniform(7500, 8500)
+  y = random.uniform(5000, 6000)
+
+  return x, y
+
+def criar_estrelas(quant_estrelas: int) -> list:
+  """
+  Cria uma lista de objetos da classe Estrela com atributos gerados aleatoriamente
+
+  Parâmetros:
+    quant_estrelas (int): Quantidade de estrelas a serem geradas
+
+  Rertorno:
+    listaEstrelas: Lista de objetos Estrela
+  """
+
+  # Lista de objetos Estrela
+  listaEstrelas = []
+
+  # Criando duas estrelas que se orbitam
+  x = (ESPACO_VIRT_LARG / 2) + 750
+  y = (ESPACO_VIRT_ALT / 2) - 375
+
+  listaEstrelas.append(Estrela(
+    x, y, # Coordendas iniciais x e y
+    -75, # Vx
+    -75, # Vy
+    7200000, # Massa
+    RAIO_ESTRELA, COR_ESTRELA
+  ))
+
+  x = (ESPACO_VIRT_LARG / 2) - 750
+  y = (ESPACO_VIRT_ALT / 2) + 375
+
+  listaEstrelas.append(Estrela(
+    x, y, # Coordendas iniciais x e y
+    75, # Vx
+    75, # Vy
+    7200000, # Massa
+    RAIO_ESTRELA, COR_ESTRELA
+  ))
+
+  return listaEstrelas
+
+def criar_planetas(quant_planetas: int) -> list:
+  """
+  Cria uma lista de objetos da classe Estrela com atributos gerados aleatoriamente
+
+  Parâmetros:
+    quant_planetas (int): Quantidade de planetas a serem gerados
+
+  Rertorno:
+    listaPlanetas: Lista de objetos Planeta
+  """
+
+  # Lista de objetos Planeta
+  listaPlanetas = []
+
+  # Criando um planeta com os parâmetros definidos, e adicionando ela à lista
+  for _ in range(quant_planetas):
+    x, y = gerar_posicao_aleatoria()
+
+    listaPlanetas.append(Planeta(
+      x, y, # Coordendas iniciais x e y
+      -75, # Vx
+      150, # Vy
+      100, # Massa
+      RAIO_PLANETA, COR_PLANETA
+    ))
+
+  return listaPlanetas
+
+def interacao_corpos(corpoA, corpoB : CorpoCeleste):
+  """
+  Calcula a interação gravitacional entre dois corpos e altera as velocidades dos corpos de acordo
+
+  Parâmetros:
+  corpoA (CorpoCeleste): Primeiro corpo envolvido
+  corpoB (CorpoCeleste): Segundo corpo envolvido
+
+  Retorno:
+
+  """
+
+  # Calculando a distância entre os dois corpos
+  distX = corpoB.x - corpoA.x
+  distY = corpoB.y - corpoA.y
+  distanciaAB = math.sqrt(distX ** 2 + distY ** 2)
+
+  # Para evitar uma divisão por zero, verificamos se a distância é maior que um limite mínimo
+  if distanciaAB > 0.01:
+    # Calculando o valor da força gravitacional entre esses dois corpos
+    forcaGrav = G * corpoA.massa * corpoB.massa / (distanciaAB ** 2)
+
+    # Calculando as acelerações de cada corpo, em cada eixo, segundo a Segunda Lei de Newton
+    ax_corpoA = forcaGrav * distX / (distanciaAB * corpoA.massa)
+    ay_corpoA = forcaGrav * distY / (distanciaAB * corpoA.massa)
+
+    ax_corpoB = -forcaGrav * distX / (distanciaAB * corpoB.massa)
+    ay_corpoB = -forcaGrav * distY / (distanciaAB * corpoB.massa)
+
+    # Atualizando as velocidades dos corpos
+    corpoA.vx += ax_corpoA
+    corpoA.vy += ay_corpoA
+
+    corpoB.vx += ax_corpoB
+    corpoB.vy += ay_corpoB
+
+def atualizar_corpos():
+  """
+  Atualiza as posições de todas as estrelas e planetas ativos no momento
+
+  Parâmetros:
+
+  Retorno: 
+
+  """
+
+  # Estrelas
+  for i in range(QUANT_ESTRELAS):
+    # Pulando estrelas inativas
+    if not estrelas[i].ativo:
+      continue
+
+    # Calculando interações entre estrelas
+    for j in range(i + 1, QUANT_ESTRELAS):
+      if estrelas[j].ativo:
+        interacao_corpos(estrelas[i], estrelas[j])
+
+    # Calculando interações entre estrelas e planetas
+    for j in range(QUANT_PLANETAS):
+      if planetas[j].ativo:
+        interacao_corpos(estrelas[i], planetas[j])
+
+    # Atualizando as posições das estrelas
+    estrelas[i].x += estrelas[i].vx
+    estrelas[i].y += estrelas[i].vy
 
 
-    return estrelas
+  # Planetas
+  for i in range(QUANT_PLANETAS):
+    # Pulando planetas inativos
+    if not planetas[i].ativo:
+      continue
 
-# Função para criar planetas
-def criar_planetas(num_planetas):
-    planetas = []
-    for _ in range(num_planetas):
-        x, y = gerar_posicao_aleatoria()
-        planetas.append(Planeta(x, y, 250, 250, 1000, 500))
-    return planetas
+    """
+    # Calculando interações entre planetas
+    for j in range(i + 1, QUANT_PLANETAS):
+      if planetas[j].ativo:
+        interacao_corpos(planetas[i], planetas[j])
+    """
+        
+    # Atualizando as posições dos planetas
+    planetas[i].x += planetas[i].vx
+    planetas[i].y += planetas[i].vy
 
-# Criação das estrelas e planetas
-estrelas = criar_estrelas(NUM_ESTRELAS)
-planetas = criar_planetas(NUM_PLANETAS)
+def desenhar_objeto(x_virtual: float, y_virtual: float, raio: float, cor):
+  # Converte as coordenadas do espaço virtual para o espaço da tela (com escala)
+  x_tela = x_virtual / ESCALA
+  y_tela = y_virtual / ESCALA
+  raio_tela = raio / ESCALA
 
-def verificar_colisoes():
-    for i in range(NUM_ESTRELAS):
-        if not estrelas[i].ativo:
-            continue
-        for j in range(i + 1, NUM_ESTRELAS):
-            if not estrelas[j].ativo:
-                continue
+  # Desenha o círculo na tela
+  pg.draw.circle(tela, cor, (int(x_tela), int(y_tela)), int(raio_tela))
 
-            dx = estrelas[j].x - estrelas[i].x
-            dy = estrelas[j].y - estrelas[i].y
-            distancia = math.sqrt(dx**2 + dy**2)
+def desenhar_corpos():
+  """
+  Desenha todos os corpos ativos na tela.
+    
+  Parâmetros:
 
-            if distancia <= (estrelas[i].raio + estrelas[j].raio):
-                # Colisão detectada - combinar estrelas
-                if estrelas[i].massa >= estrelas[j].massa:
-                    # Conservação do momento linear
-                    massa_total = estrelas[i].massa + estrelas[j].massa
-                    estrelas[i].vx = (estrelas[i].vx * estrelas[i].massa + estrelas[j].vx * estrelas[j].massa) / massa_total
-                    estrelas[i].vy = (estrelas[i].vy * estrelas[i].massa + estrelas[j].vy * estrelas[j].massa) / massa_total
-                    
-                    # Aumentar a massa e "absorver" a outra estrela
-                    estrelas[i].massa = massa_total
-                    estrelas[i].raio = (estrelas[i].raio**3 + estrelas[j].raio**3)**(1/3)  # Ajuste do raio pelo volume
-                    estrelas[j].ativo = False  # Desativar a estrela "absorvida"
-                else:
-                    # O mesmo processo, mas para o caso onde a estrela j absorve a estrela i
-                    massa_total = estrelas[j].massa + estrelas[i].massa
-                    estrelas[j].vx = (estrelas[j].vx * estrelas[j].massa + estrelas[i].vx * estrelas[i].massa) / massa_total
-                    estrelas[j].vy = (estrelas[j].vy * estrelas[j].massa + estrelas[i].vy * estrelas[i].massa) / massa_total
+  Retorno:
 
-                    estrelas[j].massa = massa_total
-                    estrelas[j].raio = (estrelas[j].raio**3 + estrelas[i].raio**3)**(1/3)  # Ajuste do raio pelo volume
-                    estrelas[i].ativo = False
+  """
+  for estrela in estrelas:
+    if estrela.ativo:
+      # Desenha a estrela na tela com sua cor original
+      desenhar_objeto(estrela.x, estrela.y, estrela.raio, estrela.cor)
 
+  for planeta in planetas:
+    if planeta.ativo:
+      # Desenha o planeta na tela com sua cor original
+      desenhar_objeto(planeta.x, planeta.y, planeta.raio, planeta.cor)
 
+### FUNÇÕES ###
 
-# Função para calcular a gravidade entre duas estrelas
-def calcular_gravidade_estrela(a, b):
-    dx = b.x - a.x
-    dy = b.y - a.y
-    distancia = math.sqrt(dx * dx + dy * dy)
+### SIMULAÇÃO ###
+rodando = True # Flag para controlar o loop principal
+sim_ativa = False # Flag para controlar a simulação
 
-    if distancia > 0.01:  # Evitar divisão por zero
-        # Força gravitacional
-        forca = G * a.massa * b.massa / (distancia * distancia)
-
-        # Aceleração no eixo X e Y para a (direção oposta à estrela b)
-        ax_a = forca * dx / distancia / a.massa
-        ay_a = forca * dy / distancia / a.massa
-
-        # Aceleração no eixo X e Y para b (direção oposta à estrela a)
-        ax_b = -forca * dx / distancia / b.massa
-        ay_b = -forca * dy / distancia / b.massa
-
-        # Atualizar as velocidades das estrelas
-        a.vx += ax_a
-        a.vy += ay_a
-
-        b.vx += ax_b
-        b.vy += ay_b
-import random
-
-# Função para atualizar a posição das estrelas
-def atualizar_estrelas():
-    for i in range(NUM_ESTRELAS):
-        if not estrelas[i].ativo:
-            continue
-
-        # Calcular a interação gravitacional com as outras estrelas
-        for j in range(i + 1, NUM_ESTRELAS):
-            if estrelas[j].ativo:
-                calcular_gravidade_estrela(estrelas[i], estrelas[j])
-
-        # Atualizar a posição das estrelas com base na velocidade
-        estrelas[i].x += estrelas[i].vx
-        estrelas[i].y += estrelas[i].vy
-
-def atualizar_planetas():
-    for i in range(NUM_PLANETAS):
-        if planetas[i].ativo:
-            for j in range(NUM_ESTRELAS):
-                dx = estrelas[j].x - planetas[i].x
-                dy = estrelas[j].y - planetas[i].y
-                distancia = math.sqrt(dx**2 + dy**2)
-                if distancia > 0:
-                    F = G * estrelas[j].massa * planetas[i].massa / (distancia**2)
-                    ax = F * dx / distancia / planetas[i].massa
-                    ay = F * dy / distancia / planetas[i].massa
-                    planetas[i].vx += ax
-                    planetas[i].vy += ay
-            planetas[i].x += planetas[i].vx
-            planetas[i].y += planetas[i].vy
-
-def desenhar_objeto(x_virtual, y_virtual, raio, cor):
-    # Converte as coordenadas do espaço virtual para o espaço da tela (com escala)
-    x_tela = x_virtual / ESCALA
-    y_tela = y_virtual / ESCALA
-    raio_tela = raio / ESCALA
-
-    # Desenha o círculo na tela
-    pygame.draw.circle(tela, cor, (int(x_tela), int(y_tela)), int(raio_tela))
-    #pygame.draw.rect(tela, cor, pygame.Rect(int(x_tela), int(y_tela), int(raio_tela*2), int(raio_tela*2)))
-
-
-def desenhar_estrelas():
-    for estrela in estrelas:
-        if estrela.ativo:
-            desenhar_objeto(estrela.x, estrela.y, estrela.raio, (255, 255, 0))  # Amarelo
-
-def desenhar_planetas():
-    for planeta in planetas:
-        if planeta.ativo:
-            desenhar_objeto(planeta.x, planeta.y, planeta.raio, (170, 170, 170))  # Branco
-
-# Loop principal
-rodando = True
 while rodando:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            rodando = False
+  # Configuração de FPS
+  tempo_delta = clock.tick(60) / 1000.0 # Limitando a 60 o FPS
 
-    tela.fill((0, 0, 0))  # Preencher a tela com preto
-    verificar_colisoes()
-    atualizar_estrelas()
-    atualizar_planetas()
-    desenhar_planetas()
-    desenhar_estrelas()
+  # Processando eventos do pygame
+  for evento in pg.event.get():
+    # Fechar a janela
+    if evento.type == pg.QUIT:
+      rodando = False
 
-    pygame.display.flip()
-    clock.tick(120)  # 60 FPS
+    # Atualizando os valores de acordo com os slider
+    if evento.type == pg_gui.UI_HORIZONTAL_SLIDER_MOVED:
+      if evento.ui_element == slider_estrelas:
+        rot_slider_estrelas.set_text(str(int(slider_estrelas.get_current_value())))
+      elif evento.ui_element == slider_planetas:
+        rot_slider_planetas.set_text(str(int(slider_planetas.get_current_value())))
 
-pygame.quit()
+    # Processando o botão de iniciar
+    if evento.type == pg.USEREVENT:
+      if evento.user_type == pg_gui.UI_BUTTON_PRESSED:
+        if evento.ui_element == botao_iniciar:
+          # Iniciando simulação
+          atualizar_informacoes()
+          estrelas = criar_estrelas(QUANT_ESTRELAS)
+          planetas = criar_planetas(QUANT_PLANETAS)
+          sim_ativa = True
+
+    # Processando eventos da GUI
+    gui.process_events(evento)
+  
+  # Atualizando GUI
+  gui.update(tempo_delta)
+
+  # Fundo preto
+  tela.fill((0, 0, 0))
+
+  if sim_ativa:
+    atualizar_corpos()
+    desenhar_corpos()
+
+  # Fazendo a GUI aparecer
+  gui.draw_ui(tela)
+
+  # Atualizando a tela
+  pg.display.flip()
+
+pg.quit()
+### SIMULAÇÃO ###
